baf0ad61a0d94f1faf2d46222c4b7a79239825fb
jelilking <abduljelilozigi@gmail.com>
2024-02-28 10:26:45 +0100

    initial commit

---
+++ scripts/forcast.js
@@ -0,0 +1,23 @@
+const key = "GI9n14hbnZdn1AUhi3eIGxEhTedUeMO2";
+
+// Get Weather Information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+//  Get city Information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0]; // Return the first city that matches the search term
+};
